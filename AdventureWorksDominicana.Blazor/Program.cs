31338185ccf37b743d10029da5187f6891589014
Jeremy Sanchez Cabrera <jeremy_sanchez@ucne.edu.do>
2026-03-23 19:08:21 -0400

    Elimina la inyeccion de Blazored en el Program.

--- AdventureWorksDominicana.Blazor/Program.cs
+++ AdventureWorksDominicana.Blazor/Program.cs
@@ -23,11 +23,8 @@ builder.Services.AddScoped<PersonService>();
 builder.Services.AddScoped<ShiftService>();
 builder.Services.AddScoped<EmployeeService>();
 builder.Services.AddScoped<ContactTypeService>();
-builder.Services.AddScoped<EmployeeService>(); 
+builder.Services.AddScoped<VendorService>(); 
 builder.Services.AddScoped<ProductCategoryService>();
-
-
-builder.Services.AddBlazoredToast();
 builder.Services.AddScoped<PhoneNumberTypeService>();
 builder.Services.AddScoped<ShiftService>();
 builder.Services.AddScoped<EmployeeService>();
